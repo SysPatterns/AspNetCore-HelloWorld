b0a047717256e7e39e36faf4fb5bc2f280b78e14
aashdown <aashdown@net-artz.co.uk>
2019-08-20 03:09:30 +0100

    adding basic MVC web app example

---
+++ src/HelloWorld.MVCWebApp/Models/Home/IndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.MVCWebApp.Models.Home
+{
+    public class IndexViewModel
+    {
+        public string Name { get; set; }
+
+        public IndexViewModel()
+        {
+            Name = "World";
+        }
+    }
+}
